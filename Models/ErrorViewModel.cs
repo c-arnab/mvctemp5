e9abf1f77412244b5a2e5e3279b5e1d9a472014b
c-arnab <arnab@programmer.net>
2021-03-16 10:09:36 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductAvailabilityService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
